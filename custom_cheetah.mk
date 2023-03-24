ff3f0e8b9dcfeea99c59a5471cac1903607224a1
hurtcopain <jacksummers715@gmail.com>
2023-03-23 23:20:44 -0400

    enable some extra flags

--- custom_cheetah.mk
+++ custom_cheetah.mk
@@ -2,6 +2,23 @@
 TARGET_SCREEN_HEIGHT := 3120
 TARGET_SCREEN_WIDTH := 1440
 
+TARGET_FLATTEN_APEX := false
+TARGET_INCLUDE_CARRIER_SETTINGS := true
+TARGET_USE_GOOGLE_TELEPHONY := true
+
+TARGET_INCLUDE_CAMERA_GO := true
+TARGET_SUPPORTS_LILY_EXPERIENCE := true
+TARGET_INCLUDE_ADDITIONAL_GAPPS := true
+
+# Inherit from telephony config
+$(call inherit-product, vendor/pixel-additional/configs/telephony.mk)
+
+# Inherit from apex config
+$(call inherit-product, vendor/pixel-additional/configs/apex.mk)
+
+# Inherit from gapps config
+$(call inherit-product, vendor/pixel-additional/gapps/config.mk)
+
 # Inherit device configuration
 $(call inherit-product, device/google/pantah/aosp_cheetah.mk)
 include device/google/pantah/device-aosp.mk
--- custom_panther.mk
+++ custom_panther.mk
@@ -2,6 +2,23 @@
 TARGET_SCREEN_HEIGHT := 2280
 TARGET_SCREEN_WIDTH := 1080
 
+TARGET_FLATTEN_APEX := false
+TARGET_INCLUDE_CARRIER_SETTINGS := true
+TARGET_USE_GOOGLE_TELEPHONY := true
+
+TARGET_INCLUDE_CAMERA_GO := true
+TARGET_SUPPORTS_LILY_EXPERIENCE := true
+TARGET_INCLUDE_ADDITIONAL_GAPPS := true
+
+# Inherit from telephony config
+$(call inherit-product, vendor/pixel-additional/configs/telephony.mk)
+
+# Inherit from apex config
+$(call inherit-product, vendor/pixel-additional/configs/apex.mk)
+
+# Inherit from gapps config
+$(call inherit-product, vendor/pixel-additional/gapps/config.mk)
+
 # Inherit device configuration
 $(call inherit-product, device/google/pantah/aosp_panther.mk)
 include device/google/pantah/device-aosp.mk
