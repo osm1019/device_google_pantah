9faefd2207f0056c93422cacf67bf29894536b0c
hurtcopain <jacksummers715@gmail.com>
2023-05-02 14:33:34 -0400

    colt os enigma

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -23,13 +23,13 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/factory_cheetah.mk \
     $(LOCAL_DIR)/aosp_panther.mk \
     $(LOCAL_DIR)/factory_panther.mk \
-    $(LOCAL_DIR)/custom_cheetah.mk \
-    $(LOCAL_DIR)/custom_panther.mk
+    $(LOCAL_DIR)/colt_cheetah.mk \
+    $(LOCAL_DIR)/colt_panther.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_cloudripper-userdebug \
     aosp_ravenclaw-userdebug \
     aosp_cheetah-userdebug \
     aosp_panther-userdebug \
-    custom_cheetah-userdebug\
-    custom_panther-userdebug
+    colt_cheetah-userdebug\
+    colt_panther-userdebug
--- colt_panther.mk
+++ colt_panther.mk
@@ -1,23 +1,17 @@
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2280
 TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOT_ANIMATION_RES := 1080
 
-TARGET_FLATTEN_APEX := false
-TARGET_INCLUDE_CARRIER_SETTINGS := true
-TARGET_USE_GOOGLE_TELEPHONY := true
+WITH_GAPPS := true
+TARGET_FACE_UNLOCK_SUPPORTED := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USES_BLUR := true
 
-TARGET_INCLUDE_CAMERA_GO := true
-TARGET_SUPPORTS_LILY_EXPERIENCE := true
-TARGET_INCLUDE_ADDITIONAL_GAPPS := true
-
-# Inherit from telephony config
-$(call inherit-product, vendor/pixel-additional/configs/telephony.mk)
-
-# Inherit from apex config
-$(call inherit-product, vendor/pixel-additional/configs/apex.mk)
-
-# Inherit from gapps config
-$(call inherit-product, vendor/pixel-additional/gapps/config.mk)
+$(call inherit-product, vendor/colt/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/pantah/aosp_panther.mk)
@@ -28,13 +22,13 @@ $(call inherit-product-if-exists, vendor/google/pixelparts/pixelparts.mk)
 $(call inherit-product-if-exists, vendor/google/pixelparts/powershare/device.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := custom_panther
+PRODUCT_NAME := colt_panther
 PRODUCT_MODEL := Pixel 7
 PRODUCT_BRAND := google
 PRODUCT_MANUFACTURER := Google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=panther \
-    PRIVATE_BUILD_DESC="panther-user 13 TQ2A.230305.008 9595452 release-keys"
+    PRIVATE_BUILD_DESC="panther-user 13 TQ2A.230405.003.E1 9802792 release-keys"
 
 BUILD_FINGERPRINT := $(PRODUCT_OVERRIDE_FINGEPRINT)
