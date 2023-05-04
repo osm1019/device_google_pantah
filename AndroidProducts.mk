0cf2419bc01f9441c1ef0bd2e61827290a5328a1
Oscar <Osm1019@gmail.com>
2023-05-03 19:18:01 -0700

    april commits plus xtended-fy it

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -23,13 +23,13 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/factory_cheetah.mk \
     $(LOCAL_DIR)/aosp_panther.mk \
     $(LOCAL_DIR)/factory_panther.mk \
-    $(LOCAL_DIR)/lineage_cheetah.mk \
-    $(LOCAL_DIR)/lineage_panther.mk
+    $(LOCAL_DIR)/xtended_cheetah.mk \
+    $(LOCAL_DIR)/xtended_panther.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_cloudripper-userdebug \
     aosp_ravenclaw-userdebug \
     aosp_cheetah-userdebug \
     aosp_panther-userdebug \
-    lineage_cheetah-userdebug\
-    lineage_panther-userdebug
+    xtended_cheetah-userdebug\
+    xtended_panther-userdebug
